05f4be3b1ab4093097d0ab465a623ef8f402d34e
xfathurrahman <contact@fathur.dev>
2024-11-04 19:26:58 +0700

    chore: configure Absolute Import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import logo from '@/assets/react.svg'
+
 function App() {
   return (
-    <div className='flex h-screen w-full items-center justify-center bg-neutral-800'>
+    <div className='flex h-screen w-full flex-col items-center justify-center bg-neutral-800'>
       <h1 className='text-3xl font-bold text-white'>Hello world!</h1>
+      <img
+        src={logo}
+        className='my-5 h-12 w-12'
+        alt='logo'
+      />
     </div>
   )
 }
