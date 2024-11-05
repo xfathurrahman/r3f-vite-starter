bdf440d2e927a1c56f953883ff6cce7466631651
xfathurrahman <contact@fathur.dev>
2024-11-05 07:49:38 +0700

    revert: remove old 3d template

--- src/components/Canvas/components/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './Gpgpu'
-export * from './Particles'
--- src/types/three.types.ts
+++
@@ -1,6 +0,0 @@
-import type { Texture, WebGLRenderTarget } from 'three'
-
-export type RenderTargetArray = [
-  WebGLRenderTarget<Texture>,
-  WebGLRenderTarget<Texture>,
-]
