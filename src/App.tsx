22ff787b945926c04b0b743d7644b7a0091d6dc0
xfathurrahman <contact@fathur.dev>
2024-11-05 07:48:37 +0700

    perf: adding PWA support with Vite-PWA 📲

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import { Canvas } from '@/components/Canvas/Canvas'
+import { Box, OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { useRegisterSW } from 'virtual:pwa-register/react'
 
 function App() {
+  useRegisterSW({
+    onRegistered(r) {
+      console.log('Service worker registered:', r)
+    },
+    onRegisterError(error) {
+      console.error('Service worker registration error:', error)
+    },
+  })
+
   return (
-    <div className='flex h-screen w-full flex-col items-center justify-center bg-neutral-800'>
-      <Canvas />
+    <div className='flex h-screen w-full flex-col items-center justify-center'>
+      <h1 className='text-white'>Hello</h1>
+      <Canvas className='h-full w-full'>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[0, 0, 0]}>
+          <meshStandardMaterial
+            attach='material'
+            color='hotpink'
+          />
+        </Box>
+        <OrbitControls />
+      </Canvas>
     </div>
   )
 }
+
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,58 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import glsl from 'vite-plugin-glsl'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), glsl()],
+  plugins: [
+    react(),
+    glsl(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: false,
+      manifest: {
+        name: 'react-vite-starter',
+        short_name: 'react-vite-starter',
+        description: 'react-vite-starter-kit',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': '/src',
